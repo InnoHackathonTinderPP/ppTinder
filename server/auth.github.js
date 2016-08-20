6d64ada7cf1dbdf6f0c5dbb44c08d466a2865e11
Nikolay Troshkov <ariser@ariser.ru>
2016-08-20 19:43:31 +0300

    fetch github data

--- server/auth.github.js
+++ server/auth.github.js
@@ -1,9 +1,31 @@
+const GITHUB_API = 'https://api.github.com';
+const APP_NAME = 'ppTinder';
+
+// Meteor.users.remove({username: 'ariser'});
+
+function githubAPICall(url, token) {
+	const options = {};
+	options.headers = {
+		//'Accept':        'application/vnd.github.v3+json',
+		'User-Agent':    APP_NAME,
+		//'Authorization': `token ${token}`
+	};
+	console.log(GITHUB_API + url);
+	console.log(options);
+	return HTTP.get(GITHUB_API + url, options);
+}
+
 Accounts.onCreateUser((options, user) => {
 	if (user.services.github) {
 		user.username = user.services.github.username;
 		user.email = user.services.github.email;
 		user.profile = options.profile;
-		// fetch guthub data .then(update data in Meteor.users)
+
+		const token = user.services.github.accessToken;
+		var result = githubAPICall(`/users/${user.services.github.username}`, token);
+		var githubUserData = result.data;
+		user.profile.avatar = githubUserData.avatar_url;
+		user.profile.githubdata = githubUserData;
 	}
 	return user;
 });
