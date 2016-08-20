1fe77c57da40c5232c0305d6456e931bd8239c0e
alex-k <alex-k@yandex-team.ru>
2016-08-20 13:02:51 +0300

    added routing

--- ppTinder.js
+++ ppTinder.js
@@ -1,8 +1,7 @@
 var Clicks = new Meteor.Collection('clicks');
+
 if (Meteor.isClient) {
   // counter starts at 0
-  Session.setDefault('counter', 0);
-
   Template.hello.helpers({
     counter: function () {
       return Clicks.find().count();
@@ -13,8 +12,9 @@ if (Meteor.isClient) {
     'click button': function () {
       // increment the counter when button is clicked
       console.log(123);
+
       Clicks.insert({ click: 123 });
-      Session.set('counter', Clicks.find().count());
+
     }
   });
 }
