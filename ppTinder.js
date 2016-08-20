bee06a1a227af56f618ec44a45422673fdf2dffe
alex-k <alex-k@yandex-team.ru>
2016-08-20 13:08:00 +0300

    pages stractures + routing

--- ppTinder.js
+++ ppTinder.js
@@ -2,13 +2,13 @@ var Clicks = new Meteor.Collection('clicks');
 
 if (Meteor.isClient) {
   // counter starts at 0
-  Template.hello.helpers({
+  Template.main.helpers({
     counter: function () {
       return Clicks.find().count();
     }
   });
 
-  Template.hello.events({
+  Template.main.events({
     'click button': function () {
       // increment the counter when button is clicked
       console.log(123);
