a4175d3c9859d814c4dbcfa835d2f1eefd07e13c
Melnikov Almaz <melnikovalmaz@gmail.com>
2016-08-20 18:57:06 +0300

    Message Generator

--- client/matches/matches.js
+++ client/matches/matches.js
@@ -5,10 +5,10 @@ Template.matches.helpers({
     messages: function () {
         var chatId = Session.get('currentChatId');
         var user = Meteor.user();
-        var chatMessages = ChatMessages.find({ chatId }).fetch();
+        var chatMessages = ChatMessages.find({chatId}).fetch();
 
         return chatMessages.map((message) => {
-            var author = Meteor.users.findOne({ _id: message.userId });
+            var author = Meteor.users.findOne({_id: message.userId});
             return {
                 authorName: author.username,
                 isMine: user._id === author._id,
@@ -20,6 +20,28 @@ Template.matches.helpers({
     }
 });
 
+Template.matches.rendered =
+    function () {
+        var chatId = Session.get('currentChatId');
+        var chat = Chats.find({chatId: chatId});
+        var user = Meteor.user();
+        var author1 = Meteor.users.findOne({_id: chat['users'][0]});
+        var author2 = Meteor.users.findOne({_id: chat['users'][1]});
+        for (var i = 0; i < 30; i++) {
+            var author = author1;
+            if (i % 4 < 3) {
+                author = author2;
+            }
+            ChatMessages.insert({
+                authorName: author.username,
+                isMine: user._id === author._id,
+                createdTime: new Date(),
+                avatar: author.profile.avatar,
+                text: "asdfasdfasdf"
+            });
+        }
+    };
+
 Template.matches.events(
     {
         'click #sendBtn': function (event) {
@@ -29,7 +51,8 @@ Template.matches.events(
                 'text': message,
                 userId: Meteor.userId(),
                 createdTime: new Date(),
-                chatId: Session.get('currentChatId')});
+                chatId: Session.get('currentChatId')
+            });
         },
 
         'submit .chat__controls': (event) => {
